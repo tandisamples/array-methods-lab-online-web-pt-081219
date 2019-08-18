19d34ca8e0a729976aed415bdd652a0821267390
Tandi Samples <tandisamples@gmail.com>
2019-08-18 21:22:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -15,7 +15,9 @@ def using_reverse(array)
 end
 
 def using_first(array)
- 
+ array = ["wow", "I", "am", "really", "learning", "arrays!"]
+ array.sort 
+ =>["I", "am", "really", "learning", "arrays!", "wow"]
 end
 
 def using_last(array)
