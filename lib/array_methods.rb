11f2136942e98ffc8adebaf081c03b4f6cc7f4ef
Tandi Samples <tandisamples@gmail.com>
2019-08-18 21:22:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -16,7 +16,7 @@ end
 
 def using_first(array)
  array = ["wow", "I", "am", "really", "learning", "arrays!"]
- array.sort 
+ array.first
  =>["I", "am", "really", "learning", "arrays!", "wow"]
 end
 
