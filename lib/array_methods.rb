cd19b9fe7e9ae10e8005d7c0f0980428da3e271a
Tandi Samples <tandisamples@gmail.com>
2019-08-18 21:20:14 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def using_include(array, element)
 end
 
 def using_sort(array)
- array = ["wow", "I", "am", "really", "learning", ]
+ array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
 
 def using_reverse(array)
